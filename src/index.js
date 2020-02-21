c07300ffa6514eeb3c08236ee1ea137923f3dd64
Prateek Jaiswal <prateekjais.dev@gmail.com>
2020-02-21 18:18:32 +0530

    Initial comment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Create a react component
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function getButtonText() {
+  return 'Click on Me';
+}
+const App = () => {
+  //   const buttonText = 'Click Me!'; allowed
+  //   const buttonText = 1234; allowed
+  const buttonText = { text: 'Click Me' };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  //   const buttonText = [10, 'Twenty', [20, 'Thirty', [10]]]; allowed
+  //   const buttonText = [10, 'Hi', { bye: 'there' }]; object not allowed inside {} jsx block
+
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {/*Can call Function*/}
+        {/* {getButtonText()} */}
+        {/*Can call variables,array.numbers but not objects*/}
+        {/* {buttonText}  */}
+        {buttonText.text} {/*allowed*/}
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+// Take the react component and show it on the screen
